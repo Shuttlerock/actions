3aa491bb441680566ad85465b119b77d4f3dab51
Dave Perrett <dave@recurser.com>
2020-09-07 09:30:22 +0900

    [ch70849] Set up eslint and prettier config, and add husky hook to format on commit.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import * as core from '@actions/core'
+
 import {wait} from './wait'
 
+/**
+ *
+ */
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
@@ -16,4 +20,4 @@ async function run(): Promise<void> {
   }
 }
 
-run()
+await run().catch(err => core.error(err))
--- src/wait.ts
+++ src/wait.ts
@@ -1,6 +1,11 @@
+/**
+ * @param milliseconds The number of milliseconds to wait.
+ *
+ * @returns A finished message.
+ */
 export async function wait(milliseconds: number): Promise<string> {
   return new Promise(resolve => {
-    if (isNaN(milliseconds)) {
+    if (Number.isNaN(milliseconds)) {
       throw new Error('milliseconds not a number')
     }
 
