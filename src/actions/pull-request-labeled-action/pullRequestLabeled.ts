e8705a9ea1df14e95921217f6385b40db2a01204
Dave Perrett <dave@shuttlerock.com>
2021-01-13 12:55:59 +0900

    [#283] [STUDIO-690] Add the 'please-review' label to dependabot pull requests, and assign reviewers.

--- src/actions/pull-request-labeled-action/pullRequestLabeled.ts
+++ src/actions/pull-request-labeled-action/pullRequestLabeled.ts
@@ -1,8 +1,13 @@
 import { info } from '@actions/core'
 import { EventPayloads } from '@octokit/webhooks'
 
-import { GithubWriteUser } from '@sr-services/Constants'
-import { addLabels } from '@sr-services/Github'
+import {
+  DependenciesLabel,
+  GithubWriteUser,
+  PleaseReviewLabel,
+} from '@sr-services/Constants'
+import { fetchRepository, User } from '@sr-services/Credentials'
+import { addLabels, assignReviewers } from '@sr-services/Github'
 
 /**
  * Runs whenever a pull request is labeled.
@@ -19,6 +24,21 @@ export const pullRequestLabeled = async (
     return
   }
 
-  const added = label?.name as string // We know we have a label for this event.
-  await addLabels(repository.name, pullRequest.number, [added])
+  let added = [label?.name as string] // We know we have a label for this event.
+
+  // If this is a dependabot PR, we want to start review straight away.
+  if (added[0] === DependenciesLabel) {
+    info('This looks like a dependency update - adding reviewers...')
+    added = [...added, PleaseReviewLabel]
+    const repo = await fetchRepository(repository.name)
+    const reviewers = repo.reviewers.map((user: User) => user.github_username)
+    if (reviewers.length > 0) {
+      info(`Assigning reviewers (${reviewers.join(', ')})...`)
+      await assignReviewers(repository.name, pullRequest.number, reviewers)
+    } else {
+      info('No reviewers to assign')
+    }
+  }
+
+  await addLabels(repository.name, pullRequest.number, added)
 }
